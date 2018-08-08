8b6d0606ce66a136a1779f1add3c65b9988123ff
Lopez Ortiz Alexandra Paola <lopezoa2018@dci25-158.ugto.mx>
2018-08-08 09:25:38 -0500

    hoy hice modificación.#

--- semana1/programa_hola.c
+++ semana1/programa_hola.c
@@ -3,3 +3,11 @@
  Este programa imprime la típica frase “hola mundo a la pantalla”
  Creado el 06/08/2018 por Alexandra Paola López Ortiz.
 */
+/*Creado el 8 de agosto de 2018 por Alexandra Lopez*/
+#include<stdio.h>
+int main()
+{
+printf("hola paola \n");
+printf("este es el primer programa del curso \n");
+}
+/* termine de escribir el programa ahhhhhhh*/
